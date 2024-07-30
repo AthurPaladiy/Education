e7ec6ea19b25ed71bc52c3a403f273b09838a0d7
ATARI <paladiyarthur@gmail.com>
2024-07-30 21:49:18 +0300

    Update module_2_4.py

--- Homeworks/Module_2/module_2_4.py
+++ Homeworks/Module_2/module_2_4.py
@@ -1,11 +1,11 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for i in range(len(numbers)):
+for i in numbers:
     is_prime = False
     division_wr_counters = 0
-    for k in range(1, numbers[i] + 2):
-        division = numbers[i] % k
+    for k in numbers:
+        division = i % k
         if division == 0:
             division_wr_counters += 1
         elif division_wr_counters == 2:
@@ -15,8 +15,8 @@ for i in range(len(numbers)):
             is_prime = False
             break
     if is_prime:
-        primes.append(numbers[i])
-    elif numbers[i] > 1:
-        not_primes.append(numbers[i])
+        primes.append(i)
+    elif i > 1:
+        not_primes.append(i)
 print('Primes:', primes)
 print('Not Primes:', not_primes)
